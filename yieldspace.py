3e5293b08c9716497b64c420b309c10e5dbfd589
Mihai <mcosma@gmail.com>
2023-03-08 14:19:49 -0500

    calc shares out given bonds in

--- yieldspace.py
+++ yieldspace.py
@@ -2,7 +2,7 @@
 from sympy.solvers import solve
 from sympy import symbols
 
-c, u, x, y, r, t, T, p = symbols("c,u,x,y,r,t,T,mp")
+c, mu, x, y, d_y, r, t, T, tau, p, z, d_z, s = symbols("c,mu,x,y,d_y,r,t,T,tau,p,z,d_z,s")
 
 print("=== Yieldspace v1 ===")
 # we have two equations for the price of the bond
@@ -24,14 +24,22 @@ print("=== Yieldspace v2 ===")
 # we have two equations for the price of the bond
 # set them equal to each other, solve for x
 #   wrong approach is using p = 1-r*t
-#      ((c * (2 * y + x)) / (u * x)) ** -T = 1-r*t
+#      ((c * (2 * y + x)) / (mu * x)) ** -T = 1-r*t
 #   correct approach is using p = 1/(1+r*t)
-#      ((c * (2 * y + x)) / (u * x)) ** -T = 1/(1+r*t)
+#      ((c * (2 * y + x)) / (mu * x)) ** -T = 1/(1+r*t)
 p3_wrong = 1 - r * t
 p3 = 1 / (1 + r * t)
-p4 = ((c * (2 * y + x)) / (u * x)) ** -T
+p4 = ((c * (2 * y + x)) / (mu * x)) ** -T
 
 sol4 = solve(p4 - p3_wrong, x)
 print(f"x_reserves (p=1-rt)\n x = {sol4}")
 sol4 = solve(p4 - p3, x)
 print(f"x_reserves (p=1/(1+rt))\n x = {sol4}")
+
+print("=== Hyperdrive  ===")
+# k_old = ( c / mu ) * (mu * z ) ^ ( 1 - tau ) + ( y + s ) ^ (1 - \tau)
+k_old = (c / mu) * (mu * x) ** (1 - t) + (y + r) ** (1 - t)
+# k_new = ( c / mu ) * ( mu * (z - d_z) ) ^ ( 1 - tau ) + ( y + s + d_y ) ^ ( 1 - tau )
+k_new = (c / mu) * (mu * (z - d_z)) ** (1 - tau) + (y + s + d_y) ** (1 - tau)
+sol5 = solve(k_new - k_old, d_z)
+print(f"calc shares out given bonds in:\n d_z = {sol5}")
